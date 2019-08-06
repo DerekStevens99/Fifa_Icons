aa63e48e4a3285648f668401957f104045c2c365
DerekStevens99 <38191099+DerekStevens99@users.noreply.github.com>
2019-08-06 10:39:33 -0400

    Add files via upload
    Rmarkdown including r code

---
+++ Fifa_EDA.R
@@ -0,0 +1,28 @@
+---
+title: "Fifa ultimate team icon analysis"
+author: "Derek"
+date: "8/5/2019"
+output: html_document
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = TRUE)
+```
+
+## Fifa 2019 Icon value analysis
+
+In 2019 fifa added a new class of players was added. Known as ICONS, these new players are a representation of legendary past players. A great example of an icon is Pele. Obviously no longer a player in the soccer world Pele has a fifa card that is meant to model his attributes as if he was playing today.
+
+```{r cars}
+summary(cars)
+```
+
+## Including Plots
+
+You can also embed plots, for example:
+
+```{r pressure, echo=FALSE}
+plot(pressure)
+```
+
+Note that the `echo = FALSE` parameter was added to the code chunk to prevent printing of the R code that generated the plot.
